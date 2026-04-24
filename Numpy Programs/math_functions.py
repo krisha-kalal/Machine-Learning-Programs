ee6042488102c9102008a280a560298fbaf8b0cd
krisha-kalal <161217181+krisha-kalal@users.noreply.github.com>
2026-04-24 10:22:46 +0530

    Created math_functions.py
    Created a simple math_functions.py file that uses numpy library in python and perform mathematical operations

---
+++ Numpy Programs/math_functions.py
@@ -0,0 +1,31 @@
+import numpy as np 
+arr = np.array([1, 2, 3, 4])
+print("square():", np.square(arr)) # Computes the element-wise square of the array.
+
+import numpy as np 
+arr = np.array([1, 4, 9, 16])
+print("sqrt():", np.sqrt(arr)) # Computes the element-wise square root of the array.
+
+import numpy as np 
+arr = np.array([1, 8, 27, 64]) 
+print("cbrt():", np.cbrt(arr)) # Computes the element-wise cube root of the array.
+
+import numpy as np 
+arr = np.array([1, 2, 3, 4]) 
+print("sum():", np.sum(arr)) # Computes the sum of the array elements.
+
+import numpy as np 
+arr = np.array([1, 5, 9, 12]) 
+print("mod():", np.mod(arr, 4)) # Computes the element-wise modulus operation with 4.
+
+import numpy as np 
+arr = np.array([1, 2, 3, 4]) 
+print("power():", np.power(arr, 3)) # Computes the element-wise power of the array elements (e.g., cubing the elements).
+
+import numpy as np 
+arr = np.array([-1, -2, 3, -4]) 
+print("absolute():", np.absolute(arr)) # Computes the element-wise absolute value of the array.
+
+import numpy as np 
+arr = np.array([-1.5, -2.5, 3.5, -4.5]) 
+print("fabs():", np.fabs(arr)) # Computes the element-wise absolute value of the array (float).
