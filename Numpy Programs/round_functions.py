00d6eb8036212ac98acd6dbdfd094aee3169b449
krisha-kalal <161217181+krisha-kalal@users.noreply.github.com>
2026-05-17 22:05:35 +0530

    Created round_functions.py

---
+++ Numpy Programs/round_functions.py
@@ -0,0 +1,14 @@
+import numpy as np 
+print("round():", np.round(0.5)) # Rounds a number to the nearest integer.
+
+import numpy as np 
+print("around():", np.around(3.5)) # Rounds a number to the nearest integer (same as `round`).
+
+import numpy as np 
+print("floor():", np.floor(9.7)) # Rounds a number down to the nearest integer.
+
+import numpy as np 
+print("ceil():", np.ceil(9.7)) # Rounds a number up to the nearest integer.
+
+import numpy as np
+print("trunc():", np.trunc(1.5)) # Truncates the decimal part of a number.
