bf2e07e6e14cdb949a753000bfde24e18df9756f
krisha-kalal <161217181+krisha-kalal@users.noreply.github.com>
2026-05-22 21:05:54 +0530

    Created constants.py

---
+++ Numpy Programs/constants.py
@@ -0,0 +1,14 @@
+import numpy as np 
+print("pi:", np.pi) # Returns the value of π (pi).
+
+import numpy as np 
+print("e:", np.e) # Returns the value of e (Euler's number).
+
+import numpy as np 
+print("inf:", np.inf) # Represents positive infinity.
+
+import numpy as np 
+print("negative inf:", -np.inf) # Represents negative infinity.
+
+import numpy as np 
+print("nan:", np.nan) # Represents "Not a Number" (NaN).
