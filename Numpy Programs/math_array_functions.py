d06caa28a940fc65f8440fa3d22e3ea8a3eaf373
krisha-kalal <161217181+krisha-kalal@users.noreply.github.com>
2026-05-22 21:05:37 +0530

    Created math_array_functions.py

---
+++ Numpy Programs/math_array_functions.py
@@ -0,0 +1,10 @@
+import numpy as np 
+c = np.array([1, 2, 3, 4]) # Creates a 1D array.
+d = np.array([5, 6, 7, 8]) # Creates another 1D array.
+print("array3:", c) 
+print("array4:", d) 
+
+print("addition:", c + d) # Adds corresponding elements of two arrays.
+print("subtraction:", d - c) # Subtracts corresponding elements of one array from another.
+print("multiplication:", c * d) # Multiplies corresponding elements of two arrays.
+print("division:", d / c) # Divides corresponding elements of one array by another.
